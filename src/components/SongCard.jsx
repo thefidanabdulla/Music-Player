61d6acffb1358c55ad5d10c916c770a072e0424b
thefidanabdulla <thefidanabdulla@gmail.com>
2022-10-09 15:04:56 +0400

    updated version

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,5 +1,13 @@
-const SongCard = () => (
-  <div>SongCard</div>
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+
+const SongCard = ({ song, i }) => (
+  <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div />
+  </div>
 );
 
 export default SongCard;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', '1ba7fc2be0msh06bafebfc9b8213p1f9f76jsn615e605deb5b');
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+  }),
+});
+
+export const {
+  useGetTopChartsQuery,
+} = shazamCoreApi;
