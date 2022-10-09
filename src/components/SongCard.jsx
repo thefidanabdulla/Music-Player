b7efe292adf02d949fa844e7c123be83145add2c
thefidanabdulla <thefidanabdulla@gmail.com>
2022-10-09 16:23:15 +0400

    play/pause functionality is done

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import PlayPause from "./PlayPause";
-import { playPause, setActiveSong } from "../redux/features/playerSlice";
+import PlayPause from './PlayPause';
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch();
   const handlePauseClick = () => {
-
+    dispatch(playPause(false));
   };
 
   const handlePlayClick = () => {
-
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
   };
 
   return (
