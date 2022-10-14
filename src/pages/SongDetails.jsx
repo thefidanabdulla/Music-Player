eef692e30e3a9f257fc2574364d577ff23a67555
thefidanabdulla <thefidanabdulla@gmail.com>
2022-10-14 16:10:35 +0400

    song details page is done

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,14 +3,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
   const dispatch = useDispatch();
   const { songid } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
-  console.log(songid);
+  const { data, isFetching: isFetchingRelatedSongs, error } = useGetSongRelatedQuery({ songid });
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
+
+  if (isFetchingSongDetails || isFetchingRelatedSongs) return <Loader title="Searching song details" />;
+
+  if (error) <Error />;
 
   return (
     <div className="flex flex-col">
@@ -24,6 +37,13 @@ const SongDetails = () => {
             )) : <p className="text-gray-400 text-base my-1">Sorry, no lyrics found!</p>}
         </div>
       </div>
+      <RelatedSongs
+        data={data}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
     </div>
   );
 };
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,10 +12,12 @@ export const shazamCoreApi = createApi({
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
   useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
 } = shazamCoreApi;
