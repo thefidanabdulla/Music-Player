7befba690133c9e3e61d3a69efdf3f21ea0db8fb
thefidanabdulla <thefidanabdulla@gmail.com>
2022-10-14 17:29:06 +0400

    around you is done

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+
+const CountryTracks = () => {
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+
+  useEffect(() => {
+    // at_CC1LIZsrkao6BD0m5NuN7Cv3Nc6sM
+    axios.get('https://geo.ipify.org/api/v2/country?apiKey=at_CC1LIZsrkao6BD0m5NuN7Cv3Nc6sM')
+      .then((res) => setCountry(res?.data?.location?.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+
+  if (isFetching && loading) return <Loader title="Loading songs around you" />;
+
+  if (error && country) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around You <span className="text-black">{country}</span>
+      </h2>
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.map((song, i) => (
+          <SongCard
+            activeSong={activeSong}
+            isPlaying={isPlaying}
+            key={song?.key}
+            song={song}
+            data={data}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CountryTracks;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -14,6 +14,7 @@ export const shazamCoreApi = createApi({
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
   }),
 });
 
@@ -22,4 +23,5 @@ export const {
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
 } = shazamCoreApi;
